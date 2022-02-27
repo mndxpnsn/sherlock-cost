79b03f84aadfbc0936c471b99b921106326f85a5
derek <harrison.derek.w@gmail.com>
2022-02-27 22:25:22 +0100

    add alternative solution

--- SherlockAndCost/main.cpp
+++ SherlockAndCost/main.cpp
@@ -8,8 +8,6 @@
 #include <iostream>
 #include <vector>
 
-using namespace std;
-
 typedef struct memo_table {
     bool is_set;
     int val;
@@ -18,8 +16,8 @@ typedef struct memo_table {
 m_table ** table;
 
 void init_table(int n) {
-    table = new m_table * [n + 1];
-    for(int i = 0; i < n + 1; ++i) {
+    table = new m_table * [n + 5];
+    for(int i = 0; i < n + 5; ++i) {
         table[i] = new m_table[2];
         table[i][0].is_set = false;
         table[i][0].val = 0;
@@ -36,7 +34,7 @@ int max_int(int a, int b) {
     return res;
 }
 
-int cost_func(vector<int> & B, int n, bool prev_is_one) {
+int cost_func(std::vector<int> & B, int n, bool prev_is_one) {
     int res = 0;
     
     if(table[n][prev_is_one].is_set) {
@@ -66,14 +64,44 @@ int cost_func(vector<int> & B, int n, bool prev_is_one) {
     return res;
 }
 
-int cost(vector<int> B) {
+int cost_func2(std::vector<int> & B, int n, bool prev_is_one) {
+    int max = 0;
+    
+    int size = (int) B.size();
+    
+    if(table[n][prev_is_one].is_set) {
+        return table[n][prev_is_one].val;
+    }
+    
+    if(n < size) {
+        if(prev_is_one) {
+            int max_ll1 = abs(B[n] - 1) + cost_func2(B, n + 1, false);
+            int max_ll2 = cost_func2(B, n + 1, true);
+            int max_loc = max_int(max_ll1, max_ll2);
+            max = max_int(max, max_loc);
+        }
+        if(!prev_is_one) {
+            int max_ll1 = abs(B[n - 1] - 1) + cost_func2(B, n + 1, true);
+            int max_ll2 = abs(B[n] - B[n - 1]) + cost_func2(B, n + 1, false);
+            int max_loc = max_int(max_ll1, max_ll2);
+            max = max_int(max, max_loc);
+        }
+    }
+    
+    table[n][prev_is_one].is_set = true;
+    table[n][prev_is_one].val = max;
+    
+    return max;
+}
+
+int cost(std::vector<int> B) {
     int res = 0;
     
     int size = (int) B.size();
     
     init_table(size);
     
-    res = max_int(cost_func(B, 1, false), cost_func(B, 1, true));
+    res = max_int(cost_func2(B, 1, false), cost_func2(B, 1, true));
     
     return res;
 }
@@ -81,7 +109,7 @@ int cost(vector<int> B) {
 int main(int argc, const char * argv[]) {
 
     //Declarations
-    vector<int> B;
+    std::vector<int> B;
     
     //Size input array B
     int size_B = 10;
